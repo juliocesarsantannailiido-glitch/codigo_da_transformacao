5ed58d9c9fb51b233d923a279a20ae7f2de453fd
julio daora <juliocesarsantannailiido@gmail.com>
2026-03-30 15:32:27 -0300

    Add files via upload

---
+++ desafio-modulo2-julio.py
@@ -0,0 +1,7 @@
+print("Bem vindo ao Python-Sistema de ensino!")
+
+gosto_aprender = str(input("O que gostaria de aprender? "))     
+
+print(f"Você respondeu: {gosto_aprender} !")
+
+print("Obrigado por participar do nosso sistema de ensino!")
